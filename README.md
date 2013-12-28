1dc793c5ed73d362feb26f3e19436f20a2660bf2
Peter Flynn <pflynn@adobe.com>
2013-12-28 15:10:02 -0800

    Highlight sample text matched by capturing group when mouseover the group match UI - now truly, accurately detects the group matched text's pos in most cases (and hopefully exits cleanly without highlighting anything in the other cases).

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ For those times when you need to [swoop in and save the day with a regular expre
 Just put your cursor on a regular expression literal and press Ctrl+E to bring up the editor. Enter test strings and see matches
 in real time as you edit the regexp.
 
-Displays regular expressions with full colored syntax highlighting, and highlights matching parentheses (intelligently ignoring
-escaped characters).
+* Displays regular expressions with full colored syntax highlighting
+* Highlights matched text in your test string
+* Shows all capturing-group matches
+* Mouseover a capturing-group match to highlight the corresponding group in the regex, and the match in your test string
+* Highlights matching parentheses (intelligently ignoring escaped characters) based on cursor position
 
 
 How to Install
--- RegexUtil.js
+++ RegexUtil.js
@@ -54,14 +54,16 @@ define(function (require, exports, module) {
      * Tokens are returned for internal reuse with findGroupInMatch().
      * @param {string} regexText
      * @param {number} groupI 1-indexed
-     * @return {{start:number, end:number, tokens:Array}?} Null if no such group. 'end' is exclusive.
+     * @return {{start:number, end:number, tokens:Array, startTokenI:number, endTokenI:number}?}
+     *      Null if no such group. 'end' is exclusive, but 'endTokenI' is inclusive.
      */
     function findGroupInRegex(regexText, groupI) {
         
         var tokens = regexTokens(regexText);
         
-        var startCh, nestLevel;
+        var startCh, startTokenI, nestLevel;
         
+        // Find start of group
         var i, atGroupI = 0, token;
         for (i = 0; i < tokens.length; i++) {
             token = tokens[i];
@@ -71,6 +73,7 @@ define(function (require, exports, module) {
                     token.groupI = atGroupI;  // recorded for later use by findGroupInMatch()
                     if (atGroupI === groupI) {
                         startCh = token.startCh;
+                        startTokenI = i;
                         nestLevel = token.nestLevel - 1;  // nestLevel reflects post-token state, so we want 1 level up
                         break;
                     }
@@ -78,11 +81,13 @@ define(function (require, exports, module) {
             }
         }
         
+        // Find end of group
         for ("ugh, jslint"; i < tokens.length; i++) {
             token = tokens[i];
             if (token.nestLevel === nestLevel) {
                 console.assert(token.style === "bracket" && token.string === ")");
-                return { start: startCh, end: token.startCh + token.string.length, tokens: tokens };
+                return { start: startCh, end: token.startCh + token.string.length, tokens: tokens,
+                         startTokenI: startTokenI, endTokenI: i };
             }
         }
         
@@ -105,19 +110,84 @@ define(function (require, exports, module) {
             return null;
         }
         
+        var tokens = groupPos.tokens;
+        
+        // We don't support groups with quantifiers yet - only the last match is captured, and we don't yet have a way to figure
+        // out the length of the uncaptures repeated matches before that.
+        var tokenAfterGroup = tokens[groupPos.endTokenI + 1];
+        if (tokenAfterGroup.style === "rangeinfo") {
+            return null;
+        }
+        
         // JS regexp results don't tell you the index of each group, only the index of the match overall.
         // Strategy is to construct a new regexp where the entire prefix of the match before the target group is wrapped in a
-        // single group, so its match length tell us the offset of the target group's match from the start of the overall match.
-        // FIXME: very simple approach that fails with group nesting or quantifiers!
-        var newRegexText = "(" + regexText.substr(0, groupPos.start) + ")" + regexText.substr(groupPos.start);
+        // single group, so its match length tells us the offset of the target group's match from the start of the overall match.
+        // In cases where the target group is nested, we wrap each nesting level's prefix in a group and then sum those prefix
+        // groups' lengths.
+        
+        // Work backwards (right to left) from target group: start with a prefix group open just to the left of it, and close
+        // that group if we encounter a "(" that forces us to step out a level, opening a new one on the other side of the "(".
+        var newRegexText = ")";
+        var nextNestLevel = groupPos.tokens[groupPos.startTokenI].nestLevel - 1;
+        var prefixGroups = [];
+        var lastGroupI = tokens[groupPos.startTokenI].groupI;
+        var i;
+        for (i = groupPos.startTokenI - 1; i >= 0; i--) {
+            var token = tokens[i];
+            if (token.style === "bracket" && token.string === "(") {
+                // Not every "(" we encounter requires terminating our prefix group - only those in the path up from the target
+                // group's nest level to the top level. E.g. in "a(b(c)d(e)f(TARGET))", only the leftmost "(" causes a break in
+                // prefix groups.
+                if (token.nestLevel === nextNestLevel) {
+                    nextNestLevel--;
+                    newRegexText = ")((" + newRegexText;
+//                    prefixGroupInsertions.push(i); // group inserted after tokens[i] in the original regexp's tokenization
+                    
+                    // This will be the index of our newly inserted group ONLY if we don't insert any more
+                    // new groups to the left of it. We'll correct for that later.
+                    prefixGroups.unshift(lastGroupI);
+                } else {
+                    newRegexText = token.string + newRegexText;
+                }
+                
+                // Update lastGroupI *after* the test above - the extra "(" we insert there is *inside* the "(" that our loop is
+                // currently at, so we want the group number after it, not its group number. (And we can't just use `token.groupI + 1`
+                // instead of tracking 'lastGroupI' since it's possible the "(" here is for a non-capturing group with no number;
+                // the "(" token we're on may have no indication of what the next/prev group numbers are).
+                if (token.groupI) {  // no group # if non-capturing
+                    lastGroupI = token.groupI;
+                }
+            } else {
+                newRegexText = token.string + newRegexText;
+            }
+        }
+        // Close final outermost/leftmost prefix group
+        newRegexText = "(" + newRegexText + regexText.substr(groupPos.start);
+        prefixGroups.unshift(lastGroupI);
+        console.assert(lastGroupI === 1);
+        
+        console.log("NewRegexText:", newRegexText);
+        
+        // Adjust each prefix group's number to account for other prefix groups preceding it
+        for (i = 1; i < prefixGroups.length; i++) {
+            prefixGroups[i] += i;
+        }
+        console.log("PrefixGroups:", prefixGroups);
+        
         var newRegex = new RegExp(newRegexText, options);
-        
         var newMatch = newRegex.exec(sampleText);
-//        console.log("NewRegex:", newRegexText, "->", newMatch);
+        console.log("NewRegex:", newRegexText, "->", newMatch);
         
-        // Target group may not be newMatch[2], since the new prefix group we created might have nested groups too. But we can
-        // easily get the target group's match length from the original `match` object, so we don't need to know its new index.
-        var start = newMatch.index + newMatch[1].length;
+        // Sum the lengths of all our prefix groups' matching strings to get the offset of the target group's match
+        var offsetFromMatchStart = 0;
+        for (i = 0; i < prefixGroups.length; i++) {
+            var groupMatch = newMatch[prefixGroups[i]];
+            if (groupMatch) {
+                offsetFromMatchStart += groupMatch.length;
+            }
+        }
+        var start = newMatch.index + offsetFromMatchStart;
+        console.log("Offset:", offsetFromMatchStart, "-> Start:", start, "End:", (start + match[groupI].length));
         return { start: start, end: start + match[groupI].length };
     }
     
